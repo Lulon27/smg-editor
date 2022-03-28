c9e1f91f155ae4ded45de72360e5b7d8d7c1e245
Andreas Wegner <andreaswegner@posteo.de>
2022-03-29 00:23:44 +0200

    Changed bracket type of include statement

--- include/RedStar/Logging.h
+++ include/RedStar/Logging.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #define RS_LOG_FN_WRAPPER(fn_name)\
 	template<typename... Args>\
